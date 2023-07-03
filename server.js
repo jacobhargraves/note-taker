a40f8829685f13de95d8a9bef9cccceaa5fcae45
Jacob Hargraves <jacobhargraves1@gmail.com>
2023-07-03 16:46:20 -0600

    change port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const { v4: uuidv4 } = require('uuid');
 const { readFromFile, readAndAppend } = require('./helpers/fsUtils');
 
-const PORT = process.env.port || 5001;
+const PORT = process.env.port || 4226;
 
 const app = express();
 
