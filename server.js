a2b24e4c039e5734293bd0ce67cf30049f452560
Jacob Hargraves <jacobhargraves1@gmail.com>
2023-07-03 08:46:39 -0600

    post request is functional.  Will add to db.json

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+const { readFromFile, readAndAppend } = require('./helpers/fsUtils');
 
 const PORT = process.env.port || 3001;
 
@@ -20,9 +22,32 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-app.get('/api/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/db/db.json'))
-);
+app.get('/api/notes', (req, res) => {
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+});
+
+app.post('/api/notes', (req, res) => {
+    // Log that a POST request was received
+    console.info(`${req.method} request received to add a note`);
+  
+    // Destructuring assignment for the items in req.body
+    const { title, text } = req.body;
+  
+    // If all the required properties are present
+    if (title && text) {
+      // Variable for the object we will save
+      const newNote = {
+        title,
+        text,
+        note_id: uuidv4(),
+      };
+  
+      readAndAppend(newNote, './db/db.json');
+      res.json(`note added successfully`);
+    } else {
+      res.error('Error in adding note');
+    }
+  });
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
