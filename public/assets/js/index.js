6444f7d128cf987d266f9a9a527b0a8058965c8a
Jacob Hargraves <jacobhargraves1@gmail.com>
2023-07-03 07:45:05 -0600

    added .then to index, created server file, installed express

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -31,6 +31,11 @@ const getNotes = () =>
     headers: {
       'Content-Type': 'application/json',
     },
+  })
+  .then((response) => response.json())
+  .then((data) => data)
+  .catch((error) => {
+    console.error('Error:', error);
   });
 
 const saveNote = (note) =>
@@ -40,7 +45,15 @@ const saveNote = (note) =>
       'Content-Type': 'application/json',
     },
     body: JSON.stringify(note),
-  });
+  })
+  .then((response) => response.json())
+  .then((data) => {
+    alert(data);
+    renderActiveNote(note);
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });;
 
 const deleteNote = (id) =>
   fetch(`/api/notes/${id}`, {
