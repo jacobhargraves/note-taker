2fa97617b321374c91316b81bf4edfddcb3d4988
Jacob Hargraves <jacobhargraves1@gmail.com>
2023-07-03 08:18:12 -0600

    added get route for db.json which returns the text in the file

--- server.js
+++ server.js
@@ -15,9 +15,13 @@ app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-// GET Route for feedback page
+// GET Route for notes page
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.get('/api/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/db/db.json'))
 );
 
 app.listen(PORT, () =>
